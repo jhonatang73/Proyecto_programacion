08f72d539253597b3438c06ad9690721d5a024bb
jhonatang73 <117460307+jhonatang73@users.noreply.github.com>
2022-11-25 09:48:38 -0500

    Update codigos.py

--- codigos.py
+++ codigos.py
@@ -53,25 +53,8 @@ import streamlit as st
 # app_state = {k: v[0] if isinstance(v, list) else v for k, v in app_state.items()} # fetch the first item in each query string as we don't have multiple values for each query string key in this example
 
 
-class MultiApp:
-    """Framework for combining multiple streamlit applications.
-    Usage:
-        def foo():
-            st.title("Hello Foo")
-        def bar():
-            st.title("Hello Bar")
-        app = MultiApp()
-        app.add_app("Foo", foo)
-        app.add_app("Bar", bar)
-        app.run()
-    It is also possible keep each application in a separate file.
-        import foo
-        import bar
-        app = MultiApp()
-        app.add_app("Foo", foo.app)
-        app.add_app("Bar", bar.app)
-        app.run()
-    """
+MultiApp:
+ 
 
     def __init__(self):
         self.apps = []
